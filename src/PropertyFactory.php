24bbb693f9efb6871d8698cac6fc9d352c8bd8f4
Steven Jones <steven.jones@computerminds.co.uk>
2020-06-19 15:47:07 +0100

    Add some documentation for property factory.

--- src/PropertyFactory.php
+++ src/PropertyFactory.php
@@ -1,19 +1,36 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: steven
- * Date: 06/06/2016
- * Time: 14:52
- */
 
 namespace ComputerMinds\CIDOC_CRM;
 
-
+/**
+ * A property factory.
+ */
 class PropertyFactory extends YamlFactory {
 
+  /**
+   * An internal cache of the loaded properties.
+   *
+   * @var array
+   *  The internally cached properties.
+   */
   protected $propertyCache = array();
+
+  /**
+   * A cache of the files available to this factory.
+   *
+   * @var array|null
+   *   The cached files.
+   */
   protected $listCache;
 
+  /**
+   * @param string $propertyName
+   *   The CRM property name to load.
+   *
+   * @return \ComputerMinds\CIDOC_CRM\Property|null
+   *   The loaded CRM Property object or null if a YAML representation cannot
+   *   be found.
+   */
   public function getProperty($propertyName) {
     if (!isset($this->propertyCache[$propertyName])) {
       // Search the Yaml directories for a matching name.
@@ -29,16 +46,27 @@ class PropertyFactory extends YamlFactory {
     return $this->propertyCache[$propertyName];
   }
 
+  /**
+   * The folder to search for properties in.
+   *
+   * @return string
+   *   The string 'property'.
+   */
   protected function getSubfolder() {
     return 'property';
   }
 
+  /**
+   * The properties available to this factory.
+   *
+   * @return string[]
+   *   An array of property names available to from this factory.
+   */
   public function listProperties() {
     if (!isset($this->listCache)) {
       $this->listCache = array_keys($this->listFiles());
     }
     return $this->listCache;
   }
-
 
 }
